43bde69c0a70e7e904cc3770d9c3e4774a770083
rybin-yu <rybin.y89@mail.ru>
2025-10-21 09:44:53 +0500

    Добавил список словарей пользователей и методы их изменения (CRUD)

--- Controllers/UserController.py
+++ Controllers/UserController.py
@@ -7,13 +7,23 @@ class UserController:
     def __init__(self):
         self.users = {}  # Словарь для хранения пользователей
         self.next_id = 1  # Счётчик для генерации ID (идентификатор пользователя)
-
+        self.list_users = [] # Список словарей
+        '''
+        [
+            {'user_id': 1, 'login': 'admin', 'password': '111111', 'role': 'admin', 'age': 35},
+            {'user_id': 2, 'login': 'user', 'password': '222222', 'role': 'user', 'status': True},
+            {'user_id': 3, 'login': 'manager', 'password': '3333', 'role': 'user'}
+        ]
+        '''
     # метод создания пользователей
     def add(self, login, password, role, **kwargs):
         # Проверка логина на уникальность
         for user_id, user_data in self.users.items():
             if user_data['login'] == login:
                 return f"Пользователь с логином {login} уже существут."
+        for user in self.list_users:
+            if user['login'] == login:
+                return f"Пользователь с логином {login} уже существут в списке."
         # Создать пользователя
         user_id = self.next_id
         self.users[user_id] = {
@@ -23,12 +33,26 @@ class UserController:
             "role": role,
             **kwargs  # дополнительные поля словаря {"gender":"М"},
         }
+        self.list_users.append(
+            {
+                "user_id": user_id,
+                "login": login,
+                "password": password,
+                "role": role,
+                **kwargs  # дополнительные поля словаря {"gender":"М"},
+            }
+        )
         self.next_id += 1
         return user_id
 
     # ВЫВЕСТИ пользователя по user_id
-    def show(self, user_id):
+    def show(self, user_id): # в словаре users
         return self.users.get(user_id)
+
+    def show_list(self,user_id): # в списке list_users
+        for row in self.list_users:
+            if row['user_id'] == user_id:
+                return row
 
     # ВЫВЕСТИ пользователя по login
     def show_login(self, login):
@@ -37,6 +61,11 @@ class UserController:
             if user_data['login'] == login:
                 return user_data
         return None
+
+    def show_list_login(self,login):
+        for user in self.list_users:
+            if user['login'] == login:
+                return user
 
     def update_user(self, user_id,
                     **kwargs):  # **kwargs - login = 'new_login' / password = 'new_pssword', role = 'new_role'
@@ -52,6 +81,21 @@ class UserController:
         user.update(kwargs)  # {"login" : 'new_login'}  user["login"] = "new_login"
         return True
 
+    def update_user_list(self,user_id, **kwargs):
+        for user in self.list_users:
+            if user['user_id'] == user_id:
+               # if 'login' in kwargs and self.show_list_login(kwargs['login']) is not None:
+               #     return f"Пользователь с логином {kwargs['login']} уже существут."
+               if 'login' in kwargs:
+                   if self.show_list_login(kwargs['login']) is not None:
+                        return f"Пользователь с логином {kwargs['login']} уже существут."
+               user.update(kwargs)
+               return True
+
+        return f'Пользователя с id {user_id} не существует в системе.'
+
+
+
     # удалить пользователя
     def delete_user(self, user_id):
         if user_id not in self.users:
@@ -64,6 +108,12 @@ class UserController:
     def auth(self,login, password):
         user = self.show_login(login)
         if user and user["password"] == password:
+            return user
+        return None
+
+    def auth_list(self,login, password):
+        user = self.show_list_login(login) # {'user_id': 4, 'login': 'manager_2', 'password': '3333', 'role': 'user'} / None
+        if user and user['password'] == password:
             return user
         return None
 
@@ -89,6 +139,17 @@ if __name__ == "__main__":
         password="3333",
         role="user",
     )
+    user_4 = user.add(
+        login="manager_2",
+        password="3333",
+        role="user",
+    )
 
-    print(user.auth('admin1','111111'))
-    print(user.users)
+
+    for row in user.list_users:
+        print(row)
+    print('-----------------------------')
+    print(user.update_user_list(4,login = 'manager_4',password = 'manager', role= 'manager'))
+    print('Аутентификация',user.auth_list('manager_4','manager'))
+    print(user.show_list(4))
+
